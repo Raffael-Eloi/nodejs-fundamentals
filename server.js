2331b2908f95d65dbad1d35b4a223b8b56a3a1d4
Raffael Eloi <raffaeleloi121@gmail.com>
2023-08-07 21:17:35 -0300

    add nodemon package

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const http = require("http");
 const port = 3000;
 
+const routes = {
+    '/': 'Alura node course',
+    '/books': `Welcome to the book's page`,
+    '/authors': `Welcome to the author's page`
+};
+
+
 const server = http.createServer((req, res) => {
+    req.
     res.writeHead(200, {'Content-Type': 'text/plan'});
-    res.end('Alura node course');
+    res.end(routes[req.url]);
 });
 
 server.listen(port, () => {
-    console.log(`Server responding on port http://localhost:${port}`);
+    console.log(`Server listening on port http://localhost:${port}`);
 });
