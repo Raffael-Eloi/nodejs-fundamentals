ed842e5e524f9a39eb587b407a2577b0163bcdd3
Raffael Eloi <raffaeleloi121@gmail.com>
2023-08-09 21:43:02 -0300

    add CRUD of books with basic express

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-import Express from "express";
+import express from "express";
 
-const app = Express();
+const app = express();
+
+app.use(express.json())
 
 const books = [
     { id: 1, "title": "The lord of the rings" },
@@ -11,12 +13,43 @@ app.get('/', (req, res) => {
     res.status(200).send('Alura node course');
 });
 
+/* Books */
+
 app.get('/books', (req, res) => {
     res.status(200).json(books);
 });
 
+app.post('/books', (req, res) => {
+    books.push(req.body);
+    res.status(201).send("Book successful saved")
+});
+
+app.put('/books/:id', (req, res) => {
+    let bookIndex = getBookIndexById(req.params.id);
+    books[bookIndex].title = req.body.title;
+    res.json(books);
+})
+
+app.delete('/books/:id', (req, res) => {
+    let bookIndex = getBookIndexById(req.params.id);
+    
+    books.splice(bookIndex, 1);
+
+    res.send(`Book with id ${req.params.id} successful removed`);
+})
+
+/* -- Books-- */
+
+/* Authors */
+
 app.get('/authors', (req, res) => {
     res.status(200).send('Welcome to the authors page');
 });
+
+/* -- Authors */
+
+function getBookIndexById(id) {
+    return books.findIndex(book => book.id == id);
+}
 
 export default app;
