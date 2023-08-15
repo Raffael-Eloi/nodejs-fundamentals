ad3ded96950880c52188827886ba8ee7cb8e45f1
Raffael Eloi <raffaeleloi121@gmail.com>
2023-08-15 18:57:47 -0300

    add CRUD of books on mongodb document structure

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -5,6 +5,17 @@ class BookController {
         res.status(200).json(await books.find())
     }
 
+    static getById = async (req, res) => {
+        let id = req.params.id;
+
+        try {
+            let book = await books.findById(id)
+            res.status(200).send(book.toJSON())
+        } catch(error) {
+            res.status(400).send({ message: error.message })
+        }
+    }
+
     static addNewBook = async (req, res) => {
         let book = new books(req.body);
 
@@ -19,16 +30,23 @@ class BookController {
     static updateById = async (req, res) => {
         let id = req.params.id;
 
-        let book = await books.findByIdAndUpdate(id, { $set: req.body });
-
-        if (book)
-            res.status(200).send({ message: "Books was successful update" })
-        else
+        try {
+            await books.findByIdAndUpdate(id, { $set: req.body });
+            res.status(200).send({message: "The book was successful updated"});
+        } catch (error) {
             res.status(500).send({ message: err.message })
+        }
+    }
 
+    static deleteById = async (req, res) => {
+        let id = req.params.id;
 
-        res.status(200).send(book.toJSON());
-
+        try {
+            await books.findByIdAndDelete(id);
+            res.status(200).send({message: "The book was successful deleted"});
+        } catch (error) {
+            res.status(500).send({ message: err.message })
+        }
     }
 }
 
--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 router
     .get("/books", BookController.getAll)
+    .get("/books/:id", BookController.getById)
     .post("/books", BookController.addNewBook)
     .put("/books/:id", BookController.updateById)
+    .delete("/books/:id", BookController.deleteById)
 
 export default router;
